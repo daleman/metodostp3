696ba8c1aeaddefbf811799da926ff1fa46a7ec7
lscoccola <luis.scoccola@gmail.com>
2013-06-16 18:34:23 +0000

    asdas

--- src_cpp/main.cpp
+++ src_cpp/main.cpp
@@ -14,9 +14,9 @@ int main( int argc, char** argv )
 
 	Reconocedor rec2( argv[1], nombre );
 
-	rec1.calcularAutovectores_QR( 100000, 30, 0.001f );
+	rec.calcularAutovectores_QR( 100, 30, 0.001f );
 
-	printf("tengo %d autovectores\n", rec1.cuantosAutovectores() );
+	printf("tengo %d autovectores\n", rec.cuantosAutovectores() );
 
 	return 0;
 }
