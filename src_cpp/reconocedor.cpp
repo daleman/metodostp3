7dd43bf9943b3dab23e88eb2d237e2997ea0d298
lscoccola <luis.scoccola@gmail.com>
2013-06-16 21:16:26 +0000

    shi

--- src_cpp/reconocedor.cpp
+++ src_cpp/reconocedor.cpp
@@ -245,7 +245,7 @@ void Reconocedor::calcularAutovectores_potencia()
 }
 
 
-int Reconocedor::reconocer_kVecinos( int cantComponentes, int k )
+int Reconocedor::reconocer_kVecinos( int cantComponentes, int k, int indice_imagen )
 {
 	if (!instanciaAbierta) {
 		printf("No se elegio instancia a evaluar\n");
@@ -254,51 +254,29 @@ int Reconocedor::reconocer_kVecinos( int cantComponentes, int k )
 
 	tcs();
 
-	vector<double> digitos(10,0.f);
-	vector<double> normas(cantComponentes,0.f);
+	if ( indice_imagen > aEvaluar->cantFil() ) {
+		printf("pediste una imagen fuera de rango\n");
+		return;
+	}
 
-	Matriz<double> autodigito_orig(cantComponentes,1); 
-	autodigito_orig.cargarMultiplicacion(Vt,imagen);
-	Matriz<double> x_i(cantComponentes,1);
-	Matriz<double> temp(cantComponentes,1);
-	Matriz<double> resta(cantComponentes,1);
-	
-	for (int i = 0; i < cantComponentes; i++)
-	{
-		x_i.submatriz(i,i,1,X.cantCol(),X);		//agarro la fila i, es decir una imagen
-		
-		temp.cargarMultiplicacion(Vt,x_i);
-		resta.cargarResta(autodigito_orig,temp);
-		normas[i] = resta.norma();
-	}
-	
-	//hacemos un k-selection sort con las normas y el vector de labels al mismo tiempo
-	// asi quedan ordenados por distancia
-	// luego en el vector digitos ( de diez elementos, uno para cada digito), guardamos las frecuencias de los
-	// primeros elementos de normas, sumando 1 cuando vemos que es uno de los primeros k de menor distancia.
-	
-	double min = 0;
-	for (int i = 0; i < cantComponentes; i++)
-	{
-		min = i;
-		for (int j = i; j < cantComponentes-1 ; j++)
-		{
-			min = ( normas [min] < normas[j+1] ) ?  min : (j+1) ;
-		}
-		double temp2 = normas [i];
-		double templ = labels[i];
-		labels[i] = labels[min];
-		normas[i] = normas[min];
-		normas[min] = temp2;
-		labels[min] = templ;
-		digitos[(labels[i])]++;
-	}
-	
-	int max = 0;
-	for (int i = 0; i < 9; i++)
-	{
-		max = ( digitos[max] > digitos[i+1] ) ?  max : (i+1);
-	}
+	int cantIm = imagenes->cantFil();
+
+	vector< double* > distancias;
+
+	Matriz<double> resta(TAMANO_IMAGEN,1);
+
+	for ( int i=0 ; i<cantIm ; ++i ) {
+
+		resta = 0;
+		for( int j=0 ; j<TAMANO_IMAGEN ; ++j ) {
+			resta[i][0] = (*aEvaluar)[indice_imagen][j] - (*imagenes)[i][j];
+			resta.norma();
+
+
+	vector< int* > frecuencias;
+
+
+
 }
 
 void Reconocedor::tcs()
