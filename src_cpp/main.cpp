14336b83e8b0c2c618bfaef040f0fe930019772b
Damian <damian@ubuntu.(none)>
2013-06-18 16:16:58 -0300

    parametor

--- src_cpp/main.cpp
+++ src_cpp/main.cpp
@@ -18,7 +18,7 @@ int main( int argc, char** argv )
 {
 	Reconocedor rec( argv[1] , argv[2] );
 
-	rec.calcularAutovectores_QR(MAXITER_QR, MAXITER_INV_POTENCIA, TOLERANCIA, atoi(argv[5]));
+	rec.calcularAutovectores_QR(MAXITER_QR, MAXITER_INV_POTENCIA, atof(argv[4]), atoi(argv[5]));
 
 	rec.abrir_instancia_a_evaluar( argv[3], 1, DIGITOS );
 
